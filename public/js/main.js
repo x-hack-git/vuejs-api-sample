7d3f09ecbbe3d52313d8c54adc8fd5f06ddcada9
EC2 Default User <ec2-user@ip-172-31-39-79.us-east-2.compute.internal>
2019-10-18 17:37:28 +0000

    todo作成

--- public/js/main.js
+++ public/js/main.js
@@ -2,13 +2,26 @@
   'use strict';
   
   var vm = new Vue({
-      el: "#app",
-      data: {
-          todos: [
-            'task 1',
-            'task 2',
-            'task 3',
-          ]
+    el: "#app",
+    data: {
+      newItem: "",
+      todos: [
+        'task 1',
+        'task 2',
+        'task 3',
+      ]
+    },
+    methods: {
+      addItem: function(e) {
+        //e.preventDefault();
+        this.todos.push(this.newItem);
+        this.newItem = "";
+      },
+      deleteItem: function(index) {
+        if(confirm('are you sure?')){
+          this.todos.splice(index, 1);
+        }
       }
+    }
   })
 })();

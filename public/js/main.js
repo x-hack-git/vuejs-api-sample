1e0ceca1329a928446534420fa693b47251a6354
EC2 Default User <ec2-user@ip-172-31-39-79.us-east-2.compute.internal>
2019-10-19 01:05:56 +0000

    todo app done

--- public/js/main.js
+++ public/js/main.js
@@ -5,22 +5,49 @@
     el: "#app",
     data: {
       newItem: "",
-      todos: [
-        'task 1',
-        'task 2',
-        'task 3',
-      ]
+      todos: []
+    },
+    watch : {
+      todos: {
+        handler: function(){
+          localStorage.setItem('todos', JSON.stringify(this.todos));
+        },
+        deep: true
+      }
+    },
+    mounted: function (argument) {
+      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
     },
     methods: {
-      addItem: function(e) {
+      addItem: function() {
         //e.preventDefault();
-        this.todos.push(this.newItem);
+        var item = {
+          title: this.newItem,
+          isDone: false
+        };
+        this.todos.push(item);
         this.newItem = "";
       },
       deleteItem: function(index) {
         if(confirm('are you sure?')){
           this.todos.splice(index, 1);
         }
+      },
+      purge: function() {
+        if(!confirm('delete finished?')){
+          return;
+        }
+        this.todos = this.todos.filter((todo) => {
+          return !todo.isDone;
+        });
+      }
+    },
+    computed: {
+      remaining: function() {
+        var items = this.todos.filter((todo) => {
+          return !todo.isDone;
+        });
+        return items.length;
       }
     }
   })
